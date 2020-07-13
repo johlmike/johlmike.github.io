b5eceedccb40ffb787babe71675f8ee8371b638d
Johlmike <johlmike@gmail.com>
2020-07-13 23:28:09 +0800

    主線任務第四週init
    登入頁面完成
    商品列表建置中

---
+++ hexschool/mainquest-4/js/index.js
@@ -0,0 +1,45 @@
+const app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      baseUrl: 'https://course-ec-api.hexschool.io/api/',
+      uuid: '0ace4b59-2bf0-443c-968c-4c9a458d1cb9',
+      adminId: '',
+      adminPw: '',
+    };
+  },
+  methods: {
+    adminLogin() {
+      // 準備request用data
+      const data = {
+        email: this.adminId,
+        password: this.adminPw,
+      };
+      // Login
+      axios.post(`${this.baseUrl}auth/login`, data)
+        .then(res => {
+          const token = res.data.token; // Token
+          const expire = new Date(res.data.expired * 1000); // 過期日
+          // 存入cookie
+          document.cookie = `moerabbitworld-token=${token}; expires=${expire};`;
+        })
+        .catch(err => {
+          // 取得error回傳的data
+          const data = err.response.data;
+          // 取得帳號的錯誤訊息
+          const idMsg = data.errors.email ? data.errors.email[0] : '';
+          // 取得密碼的錯誤訊息
+          const pwMsg = data.errors.password ? data.errors.password[0] : '';
+          alert(`${idMsg}\n${pwMsg}`);
+        });
+    }
+  },
+  created () {
+    // 元件建立時，檢查是否已有Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)moerabbitworld-token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    // 有Token的話，自動跳轉頁面
+    if( token ){
+      window.location = './products.html';
+    }
+  }
+})
---
+++ hexschool/mainquest-4/js/products.js
@@ -0,0 +1,118 @@
+// import Vue from 'vue';
+// import Vuesax from 'vuesax';
+
+// import 'vuesax/dist/vuesax.css' //Vuesax styles
+// Vue.use(Vuesax, {
+//   // options here
+// });
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    // api網址
+    baseUrl: 'https://course-ec-api.hexschool.io/api/',
+    // 我的uuid
+    uuid: '0ace4b59-2bf0-443c-968c-4c9a458d1cb9',
+    // 驗證用token
+    token: '',
+    // 空product物件
+    productTemplate: {
+      title: "",
+      category: "",
+      content: "",
+      description: "",
+      imageUrl: "",
+      enabled: true,
+      origin_price: 0,
+      price: 0,
+      unit: "",
+      stock: 0,
+      option: {
+        comments: [],
+        hot: true
+      }
+    },
+    // 商品列表
+    products: [],
+    // 編輯中的商品
+    editingProduct: {
+      title: "",
+      category: "",
+      content: "",
+      description: "",
+      imageUrl: "",
+      enabled: true,
+      origin_price: 0,
+      price: 0,
+      unit: "",
+      stock: 0,
+      option: {
+        comments: [],
+        hot: true
+      }
+    },
+    // 編輯中的編號
+    editingIndex: 0,
+    // 因新增和編輯共用Modal，故多一個變數，確認觸發之modal是「新增商品」或「編輯商品」
+    // 之後或許調整為將modal元件獨立
+    creating: true,
+  },
+  methods: {
+    getAllProducts() {
+      const url = `${this.baseUrl}${this.uuid}/admin/ec/products`;
+      axios.get(url)
+        .then(res => {
+          // 將商品列表放入元件的data
+          this.products = res.data.data;
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err.response);
+        });
+    },
+    createProduct() {
+      this.products.push(_.cloneDeep(this.editingProduct));
+      this.editingProduct = _.cloneDeep(this.productTemplate);
+      $('#productModal').modal('hide');
+    },
+    updateProduct() {
+      Object.keys(this.products[this.editingIndex]).forEach(key => {
+        this.products[this.editingIndex][key] = _.cloneDeep(this.editingProduct[key]);
+      });
+      $('#productModal').modal('hide');
+    },
+    deleteProduct(index) {
+      this.products.splice(index, 1);
+    },
+    toggleModal(index) {
+      if (index === "new") {
+        this.creating = true;
+        // 給予一個空的資料物件
+        this.editingProduct = _.cloneDeep(this.productTemplate);
+      } else {
+        this.creating = false;
+        this.editingIndex = index;
+        // 將要編輯的商品內容放入editProduct
+        this.editingProduct = _.cloneDeep(this.products[index]);
+      }
+      $('#productModal').modal('show');
+    },
+    toggleHot(index, hot) {
+      this.products[index].option.hot = hot;
+    }
+  },
+  created() {
+    // 元件建立時，嘗試取得Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)moerabbitworld-token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    // 有Token的話，存入Token，並設定Authorization
+    if (token) {
+      this.token = token;
+      // 設定所有axios request的header的Authorization為使用者之token
+      axios.defaults.headers.common.Authorization = `Bearer ${this.token}`;
+      // 取得所有商品列表
+      this.getAllProducts();
+    } else { // 沒有的話，跳轉回Login頁面
+      window.location = './index.html';
+    }
+  }
+});

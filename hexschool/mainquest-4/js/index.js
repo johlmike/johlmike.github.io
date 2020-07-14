d7a08f817581f4735ee4d2a7e314ca2179af8527
Johlmike <johlmike@gmail.com>
2020-07-14 17:49:00 +0800

    製作登入時的讀取畫面

--- hexschool/mainquest-4/js/index.js
+++ hexschool/mainquest-4/js/index.js
@@ -1,15 +1,22 @@
+import LoadingPage from '../components/LoadingPage.js';
+
 const app = new Vue({
   el: '#app',
+  components: {
+    LoadingPage,
+  },
   data() {
     return {
       baseUrl: 'https://course-ec-api.hexschool.io/api/',
       uuid: '0ace4b59-2bf0-443c-968c-4c9a458d1cb9',
       adminId: '',
       adminPw: '',
+      isLoading: false,
     };
   },
   methods: {
     adminLogin() {
+      this.isLoading = true; // 讀取中
       // 準備request用data
       const data = {
         email: this.adminId,
@@ -18,6 +25,7 @@ const app = new Vue({
       // Login
       axios.post(`${this.baseUrl}auth/login`, data)
         .then(res => {
+          this.isLoading = false; // 讀取結束
           const token = res.data.token; // Token
           const expire = new Date(res.data.expired * 1000); // 過期日
           // 存入cookie
@@ -25,6 +33,7 @@ const app = new Vue({
           window.location = './products.html';
         })
         .catch(err => {
+          this.isLoading = false; // 讀取結束
           // 取得error回傳的data
           const data = err.response.data;
           // 取得帳號的錯誤訊息

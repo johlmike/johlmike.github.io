6b4390cf3816fe0443289e7f35af21e0cfa859f1
Johlmike <johlmike@gmail.com>
2020-07-15 04:00:53 +0800

    調整分頁按鈕顯示

--- hexschool/mainquest-4/components/Pagination.js
+++ hexschool/mainquest-4/components/Pagination.js
@@ -1,24 +1,22 @@
 export default {
   template: `
-  <div class="row">
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item" @click.prevent="previousPage">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-        <li :class="{'page-item': true, active: page === currentPage}" v-for="page in totalPage" :key="'page_' + page" @click.prevent="changePage(page)">
-          <a class="page-link" href="#">{{ page }}</a>
-        </li>
-        <li class="page-item" @click.prevent="nextPage">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination mb-0">
+      <li class="page-item" @click.prevent="previousPage">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li :class="{'page-item': true, active: page === currentPage}" v-for="page in totalPage" :key="'page_' + page" @click.prevent="changePage(page)">
+        <a class="page-link" href="#">{{ page }}</a>
+      </li>
+      <li class="page-item" @click.prevent="nextPage">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
   `,
   props: {
     totalPage: Number,

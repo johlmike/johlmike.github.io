45213db184187cd3d704174fd395a1bcdbd3b430
Johlmike <johlmike@gmail.com>
2020-07-15 17:54:49 +0800

    修改modal溝通方式
    原本不知道為什麼v-model可以修改父元件的資料，修改為modal修改的資料會先存在EditingModal元件內，儲存或創建時才會透過事件傳回父元件

--- hexschool/mainquest-4/components/EditingModal.js
+++ hexschool/mainquest-4/components/EditingModal.js
@@ -10,40 +10,40 @@ export default {
                 <div class="col-sm-4">
                   <!-- 一次最多上傳五張圖片 -->
                   <template v-for="index in 5">
-                    <div class="form-group" :key="product.id + '_' + index">
+                    <div class="form-group" :key="editingProduct.id + '_' + index">
                       <label :for="'imageUrl' + '_' + index">圖片網址</label>
                       <input type="text" :id="'imageUrl' + '_' + index" class="form-control"
-                        v-model.lazy="product.imageUrl[index-1]"
-                        @change.lazy="product.imageUrl = filterImageUrl">
+                        v-model.lazy="editingProduct.imageUrl[index-1]"
+                        @change.lazy="editingProduct.imageUrl = filterImageUrl">
                     </div>
-                    <img v-show="product.imageUrl[index-1]" :src="product.imageUrl[index-1]"
-                      :alt="product.title + '_' +  index" class="img-fluid mb-2">
+                    <img v-show="editingProduct.imageUrl[index-1]" :src="editingProduct.imageUrl[index-1]"
+                      :alt="editingProduct.title + '_' +  index" class="img-fluid mb-2">
                   </template>
                 </div>
                 <div class="col-sm-8">
                   <div class="form-group">
                     <label for="title">商品名稱</label>
-                    <input type="text" name="title" id="title" class="form-control" v-model="product.title">
+                    <input type="text" name="title" id="title" class="form-control" v-model="editingProduct.title">
                   </div>
                   <div class="form-row">
                     <div class="col">
                       <div class="form-group">
                         <label for="category">商品分類</label>
                         <input type="text" name="category" id="category" class="form-control"
-                          v-model="product.category">
+                          v-model="editingProduct.category">
                       </div>
                     </div>
                     <div class="col">
                       <div class="form-group">
                         <label for="stock">庫存</label>
                         <input type="number" name="stock" id="stock" class="form-control"
-                          v-model.number="product.options.stock" min="0">
+                          v-model.number="editingProduct.options.stock" min="0">
                       </div>
                     </div>
                     <div class="col">
                       <div class="form-group">
                         <label for="unit">單位</label>
-                        <input type="text" name="unit" id="unit" class="form-control" v-model="product.unit">
+                        <input type="text" name="unit" id="unit" class="form-control" v-model="editingProduct.unit">
                       </div>
                     </div>
                   </div>
@@ -52,14 +52,14 @@ export default {
                       <div class="form-group">
                         <label for="origin_price">原價</label>
                         <input type="number" name="origin_price" id="origin_price" class="form-control"
-                          v-model.number="product.origin_price" min="0">
+                          v-model.number="editingProduct.origin_price" min="0">
                       </div>
                     </div>
                     <div class="col">
                       <div class="form-group">
                         <label for="price">售價</label>
                         <input type="number" name="price" id="price" class="form-control"
-                          v-model.number="product.price"
+                          v-model.number="editingProduct.price"
                           min="0">
                       </div>
                     </div>
@@ -69,26 +69,26 @@ export default {
                   <div class="form-row">
                     <div class="col">
                       <label for="content">商品敘述</label>
-                      <textarea class="form-control" id="content" rows="3" v-model="product.content"></textarea>
+                      <textarea class="form-control" id="content" rows="3" v-model="editingProduct.content"></textarea>
                     </div>
                     <div class="col">
                       <label for="description">商品說明</label>
                       <textarea class="form-control" id="description" rows="3"
-                        v-model="product.description"></textarea>
+                        v-model="editingProduct.description"></textarea>
                     </div>
                   </div>
                   <div class="form-row mt-3">
                     <div class="col">
                       <div class="custom-control custom-switch">
                         <input type="checkbox" class="custom-control-input" id="enabled"
-                          v-model="product.enabled">
+                          v-model="editingProduct.enabled">
                         <label class="custom-control-label" for="enabled">是否上架？</label>
                       </div>
                     </div>
                     <div class="col">
                       <div class="custom-control custom-switch">
                         <input type="checkbox" class="custom-control-input" id="hot"
-                          v-model="product.options.hot">
+                          v-model="editingProduct.options.hot">
                         <label class="custom-control-label" for="hot">是否為熱門商品？</label>
                       </div>
                     </div>
@@ -112,11 +112,29 @@ export default {
   },
   data() {
     return {
+      editingProduct: {},
     };
   },
   methods: {
     save(mode) {
-      this.$emit('save', mode, this.product);
+      this.$emit('save', mode, this.editingProduct);
+    },
+    checkForm() {
+      let ready = true;
+      // 圖片至少上傳一張
+      // if( this. )
+      // 商品名稱 為必填項目
+      // 商品分類 為必填項目
+      // 商品敘述 為必填項目
+      // 商品說明 為必填項目
+    }
+  },
+  created() {
+    this.editingProduct = _.cloneDeep(this.product);
+  },
+  watch: {
+    product() {
+      this.editingProduct = _.cloneDeep(this.product);
     },
   },
   computed: {

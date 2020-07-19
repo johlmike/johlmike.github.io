883015ef6c4bb806a275dad409d048f411566483
Johlmike <johlmike@gmail.com>
2020-07-19 10:17:24 +0800

    第五週主線任務init

---
+++ hexschool/mainquest-5/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import NavBar from '../components/NavBar.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    NavBar,
+  },
+};
+</script>

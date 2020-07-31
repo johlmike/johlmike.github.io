a00b78c8bbf817b9359bdbdbdf3d0cc69927ceea
Johlmike <johlmike@gmail.com>
2020-07-31 16:27:20 +0800

    修正結帳頁面

--- hexschool/mainquest-5/src/views/layout/Checkout.vue
+++ hexschool/mainquest-5/src/views/layout/Checkout.vue
@@ -1,100 +1,108 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <ValidationObserver v-slot="{ invalid }" class="w-100">
-        <form class="pb-5 col-6 offset-3">
-          <ValidationProvider rules="required" v-slot="{ errors, classes }" mode="lazy">
+  <div>
+    <NavBar :isTop="false"></NavBar>
+    <div class="container-fluid checkout">
+      <div class="row pt-3">
+        <ValidationObserver v-slot="{ invalid }" class="w-100">
+          <form class="pb-5 col-6 offset-3">
+            <ValidationProvider rules="required" v-slot="{ errors, classes }" mode="lazy">
+              <div class="form-group">
+                <label for="name">收件人姓名</label>
+                <input
+                  type="text"
+                  name="收件人姓名"
+                  id="name"
+                  v-model="userData.name"
+                  :class="classes"
+                  class="form-control"
+                />
+                <span class="invalid-feedback">{{ errors[0] }}</span>
+              </div>
+            </ValidationProvider>
+            <ValidationProvider rules="email|required" v-slot="{ errors, classes }" mode="lazy">
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input
+                  type="email"
+                  name="Email"
+                  id="email"
+                  v-model="userData.email"
+                  :class="classes"
+                  class="form-control"
+                />
+                <span class="invalid-feedback">{{ errors[0] }}</span>
+              </div>
+            </ValidationProvider>
+            <ValidationProvider rules="min:8|required" v-slot="{ errors, classes }" mode="lazy">
+              <div class="form-group">
+                <label for="tel">電話</label>
+                <input
+                  type="tel"
+                  name="電話"
+                  id="tel"
+                  v-model="userData.tel"
+                  :class="classes"
+                  class="form-control"
+                />
+                <span class="invalid-feedback">{{ errors[0] }}</span>
+              </div>
+            </ValidationProvider>
+            <ValidationProvider rules="required" v-slot="{ errors, classes }" mode="lazy">
+              <div class="form-group">
+                <label for="address">地址</label>
+                <input
+                  type="text"
+                  name="地址"
+                  id="address"
+                  v-model="userData.address"
+                  :class="classes"
+                  class="form-control"
+                />
+                <span class="invalid-feedback">{{ errors[0] }}</span>
+              </div>
+            </ValidationProvider>
             <div class="form-group">
-              <label for="name">收件人姓名</label>
-              <input
-                type="text"
-                name="收件人姓名"
-                id="name"
-                v-model="userData.name"
-                :class="classes"
-                class="form-control"
-              />
-              <span class="invalid-feedback">{{ errors[0] }}</span>
+              <label for="payment">付款方式</label>
+              <select class="form-control" id="payment" v-model="userData.payment">
+                <option value="WebATM">WebATM</option>
+                <option value="ATM">ATM</option>
+                <option value="Barcode">Barcode</option>
+                <option value="Credit">Credit</option>
+                <option value="ApplePay">ApplePay</option>
+                <option value="GooglePay">GooglePay</option>
+              </select>
             </div>
-          </ValidationProvider>
-          <ValidationProvider rules="email|required" v-slot="{ errors, classes }" mode="lazy">
             <div class="form-group">
-              <label for="email">Email</label>
-              <input
-                type="email"
-                name="Email"
-                id="email"
-                v-model="userData.email"
-                :class="classes"
+              <label for="message">留言</label>
+              <textarea
                 class="form-control"
-              />
-              <span class="invalid-feedback">{{ errors[0] }}</span>
+                id="message"
+                rows="3"
+                v-model="userData.message"
+              ></textarea>
             </div>
-          </ValidationProvider>
-          <ValidationProvider rules="min:8|required" v-slot="{ errors, classes }" mode="lazy">
-            <div class="form-group">
-              <label for="tel">電話</label>
-              <input
-                type="tel"
-                name="電話"
-                id="tel"
-                v-model="userData.tel"
-                :class="classes"
-                class="form-control"
-              />
-              <span class="invalid-feedback">{{ errors[0] }}</span>
-            </div>
-          </ValidationProvider>
-          <ValidationProvider rules="required" v-slot="{ errors, classes }" mode="lazy">
-            <div class="form-group">
-              <label for="address">地址</label>
-              <input
-                type="text"
-                name="地址"
-                id="address"
-                v-model="userData.address"
-                :class="classes"
-                class="form-control"
-              />
-              <span class="invalid-feedback">{{ errors[0] }}</span>
-            </div>
-          </ValidationProvider>
-          <div class="form-group">
-            <label for="payment">付款方式</label>
-            <select class="form-control" id="payment" v-model="userData.payment">
-              <option value="WebATM">WebATM</option>
-              <option value="ATM">ATM</option>
-              <option value="Barcode">Barcode</option>
-              <option value="Credit">Credit</option>
-              <option value="ApplePay">ApplePay</option>
-              <option value="GooglePay">GooglePay</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="message">留言</label>
-            <textarea
-              class="form-control"
-              id="message"
-              rows="3"
-              v-model="userData.message"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            class="btn btn-primary btn-submit"
-            v-on:click.prevent="sendOrder"
-            :disabled="invalid"
-          >
-            送出訂單
-          </button>
-        </form>
-      </ValidationObserver>
+            <button
+              type="submit"
+              class="btn btn-primary btn-submit"
+              v-on:click.prevent="sendOrder"
+              :disabled="invalid"
+            >
+              送出訂單
+            </button>
+          </form>
+        </ValidationObserver>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue';
+
 export default {
+  components: {
+    NavBar,
+  },
   data() {
     return {
       baseUrl: process.env.VUE_APP_BASEURL,
@@ -141,6 +149,10 @@ $Alabaster: #d8e2dc;
 $Pink: #ffcad4;
 $DarkPink: #f4acb7;
 $DarkerPink: #9d8189;
+
+.checkout {
+  margin-top: 3.5rem;
+}
 
 .form-group {
   text-align: left;
